999048d4ef60ae0389fad484d714d38434bab114
Theo Kleinendorst <theo.kleinendorst@acaciawater.com>
2018-11-16 10:21:38 +0100

    survey with info buttons

--- src/components/Survey.vue
+++ src/components/Survey.vue
@@ -12,54 +12,91 @@
 </template>
 
 <script>
-import { Survey, Model } from "survey-vue";
+import { Survey, Model, JsonObject } from "survey-vue";
 import Map from "@/components/Map.vue";
 import "survey-vue/survey.css";
 import surveyData from "@/assets/survey.json";
-import axios from 'axios';
+import axios from "axios";
+
+JsonObject.metaData.addProperty("question", "popup:text");
 
 function saveResult(result) {
-  let token = localStorage.getItem("token");
-  axios.defaults.headers.common['Authorization'] = 'JWT ' + token;
+  let token = sessionStorage.getItem("token");
+  axios.defaults.headers.common["Authorization"] = "JWT " + token;
   let survey = result.data;
   let data = {
     project: survey.name,
     location: {
       type: "Point",
-      coordinates: [
-        5.0, 52.0
-      ]
+      coordinates: [5.0, 52.0] // TODO: get from map component
     },
-    user: localStorage.getItem("user"),
+    user: sessionStorage.getItem("user"),
     survey: survey
   };
-  axios.post('http://localhost:8000/api/v1/survey/',data)
+  sessionStorage.setItem('survey', data);
+  axios
+    .post("http://localhost:8000/api/v1/survey/", data)
     .then(response => {
-      alert('Successfully saved!');
+      alert("Successfully saved!");
     })
     .catch(error => {
       alert(error);
-    })
+    });
 }
 
-let model = new Model(surveyData);
-model.onComplete.add(result =>
-{
+let newSurvey = new Model(surveyData);
+newSurvey.onComplete.add(result => {
   saveResult(result);
+});
+
+newSurvey.onAfterRenderQuestion.add(function(survey, options) {
+  if (options) {
+    console.log("onAfterRenderQuestion options", options);
+    let question = options.question;
+    if (question.popup) {
+      var btn = document.createElement("button");
+      btn.className = "infobutton";
+      btn.innerHTML = "Info";
+      var question = options.question;
+      var anchor = options.htmlElement.querySelector('h5');
+      var parent = anchor.parentNode;
+      // var span = document.createElement("span");
+      // span.innerHTML = " ";
+      // header.appendChild(span);
+      //header.appendChild(btn);
+      parent.insertBefore(btn, anchor)
+      let element = document.getElementById(options.htmlElement.id);
+      console.log("found element", element);
+    }
+  }
 });
 
 export default {
   name: "MySurvey",
   components: {
-    Survey, Map
+    Survey,
+    Map
   },
   data() {
+    let surveyData = localStorage.getItem('survey');
+    if (surveyData) {
+      surveyData = surveyData.survey
+    }
     return {
-      survey: model
+      survey: surveyData || newSurvey
     };
   }
 };
 </script>
 
 <style>
+/* .sv_row :hover {
+  border: 1px solid red;
+} */
+.infobutton {
+  /* background: rgb(7, 1, 95) !important; */
+  min-width: 20px !important;
+  padding: 0 1em !important;
+  float: right !important;
+}
 </style>
