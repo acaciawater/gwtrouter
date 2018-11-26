888eed853e8d26573934e72be00e575a8ad2daea
Theo Kleinendorst <theo.kleinendorst@acaciawater.com>
2018-11-26 09:21:42 +0100

    select indicators based on survey

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -41,6 +41,17 @@ export default {
     this.getIndicators().then(result => {
       this.indicators = result
       // console.log("Indicators: ", this.indicators)
+      let surveyData = localStorage.getItem("survey")
+      if(surveyData) {
+        console.debug('survey found:')
+        let survey = JSON.parse(surveyData).survey
+        console.debug(survey)
+        let selected = this.selectIndicators(survey)
+        this.indicators.forEach(indicator => {
+          indicator.selected = selected.has(indicator.name)
+        })
+      console.log("Indicators: ", this.indicators)
+      }
     })
   },
   methods: {
@@ -49,6 +60,49 @@ export default {
         .then(response => {
           return response.data
         })
+    },
+    selectIndicators(data) {
+      // get indicators for survey
+      let indicators = new Set()
+      if(data.usegw==="yes") {
+        indicators.add("Groundwater stress")
+        indicators.add("Recharge")
+        indicators.add("Population growth")
+        indicators.add("Proximity to wetlands")
+        if(data.watersector==="Drinking water") {
+          indicators.add("Arsenic")
+          indicators.add("Fluoride")
+          indicators.add("Salinity")
+          indicators.add("Proximity to the coast")
+        }
+        else if(data.watersector==="Irrigation" || data.watersector=="Livestock") {
+          indicators.add("Arsenic")
+          indicators.add("Salinity")
+          indicators.add("Proximity to the coast")
+        }
+        else if(data.watersector==="Industry") {
+          indicators.add("Salinity")
+          indicators.add("Proximity to the coast")
+          if(data.industry_type==="Food") {
+            indicators.add("Arsenic")
+            indicators.add("Fluoride")
+          }
+        }
+      }
+      if(data.generates_waste==="yes") {
+        indicators.add("Vulnerability")
+      }
+      if(data.surface==="yes") {
+        indicators.add("Population density upstream") // not yet available?
+        indicators.add("Return flow ratio")
+      }
+      if(data.landuse==="yes") {
+        // add test recharge reduction
+        indicators.add("Recharge")
+        indicators.add("Groundwater stress")
+        indicators.add("Downstream vulnerable") // not available?
+      }
+      return indicators
     }
   }
 }
